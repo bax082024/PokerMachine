2b24b980a62664644c2876246dcae19cfc4141c2
bax082024 <bax082024@gmail.com>
2025-01-02 20:40:25 +0100

    added de ck class

--- Deck.cs
+++ Deck.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace PokerMachine
 {
-    internal class Deck
+    public class Deck
     {
+        private List<Card> cards = new List<Card>();
+
+        public Deck()
+        {
+            string[] suits = { "heart", "diamond", "club", "spades" };
+            string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k", "ace" };
+
+            foreach (var suit in suits)
+            {
+                foreach (var rank in ranks)
+                {
+                    cards.Add(new Card(rank, suit));
+                }
+            }
+        }
+
+
     }
 }
